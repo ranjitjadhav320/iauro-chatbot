1041efbd6faf0f141d93306ec5583d83e2351c21
ranjit jadhav <ranjit.jadhav@iauro.com>
2023-07-12 12:27:02 +0530

    ranjit: init chatbot ms

---
+++ src/chatbot/chatbot.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+import { CreateChatbotDto } from './dto/create-chatbot.dto';
+import { UpdateChatbotDto } from './dto/update-chatbot.dto';
+
+const { ActivityHandler, BotFrameworkAdapter } = require('botbuilder');
+
+class MyBot extends ActivityHandler {
+  constructor() {
+    super();
+
+    this.onMessage(async (context, next) => {
+      await context.sendActivity('Hello from the chatbot!');
+      await next();
+    });
+  }
+}
+
+const bot = new MyBot();
+
+@Injectable()
+export class ChatbotService {
+  create(createChatbotDto: CreateChatbotDto) {
+    return 'This action adds a new chatbot';
+  }
+
+  findAll() {
+    return `This action returns all chatbot`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} chatbot`;
+  }
+
+  update(id: number, updateChatbotDto: UpdateChatbotDto) {
+    return `This action updates a #${id} chatbot`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} chatbot`;
+  }
+
+  chatBot() {
+    console.log("inside.....API")
+    const adapter = new BotFrameworkAdapter({
+      appId: "f0cc92a4-2765-4501-94e4-26360eea4fe5",
+      appPassword: "yA8Q~dLrRujNPA3oKduLjiqGqN84kiQDb06oc1N"
+    });
+
+    adapter.processActivity(async (context) => {
+      console.log("context", context);
+      await bot.run(context);
+    });
+    return 'This action adds a new chatbot';
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
+import * as config from 'config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({});
+
+  const swaggerConfig = new DocumentBuilder()
+    // .addServer(config.swagger.basepath)
+    // .setBasePath(config.swagger.basepath)
+    .setTitle(config.swagger.title)
+    .setDescription(config.swagger.description)
+    .setVersion(config.swagger.version)
+    .addTag(config.swagger.tag)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(config.server.port);
+}
+
+
+bootstrap();
